c55a5d42693fb6ca7db76a33adcd123af21f0d3a
OvsyankinVI <ovsyankin717@gmail.com>
2023-03-22 00:56:09 +0500

    230321_Add header, p-1, p2 and start p-3

---
+++ js/main.js
@@ -0,0 +1,11 @@
+/* document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems);
+    var modal = document.querySelectorAll('.modal');
+    var instances_modal = M.Modal.init(modal);
+  }); */
+
+  document.addEventListener('DOMContentLoaded', function() {
+    let car = document.querySelectorAll('.carousel');
+    let instances = M.Carousel.init(car);
+  });
